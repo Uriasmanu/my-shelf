2d0810c3ec9968d6d8acfe02ef2ebf951d6fdae8
Manoela Urias <maanoelaurias@gmail.com>
2026-01-03 16:07:43 -0300

    corrige propriedade

--- components/CardBiblioteca.tsx
+++ components/CardBiblioteca.tsx
@@ -1,18 +1,17 @@
-import Image from "next/image";
 
 interface CardBibliotecaProps {
     title: string;
     author: string;
     rating: number;
-    linkedImage: string;
+    imageUrl: string;
 }
 
-export default function CardBiblioteca({ title, author, rating, linkedImage }: CardBibliotecaProps) {
+export default function CardBiblioteca({ title, author, rating, imageUrl }: CardBibliotecaProps) {
     return (
 
         <div className="bg-white p-3 rounded-xl border border-slate-100 shadow-sm cursor-pointer hover:-translate-y-1 transition">
-            <Image
-                src={linkedImage}
+            <img
+                src={imageUrl}
                 className="w-full aspect-2/3 object-cover rounded-lg mb-3"
                 alt=""
             />
